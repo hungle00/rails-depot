070b672cdbf7540da44d33960c4dc79e4f19c1d7
hungle00 <hungle0903>
2020-10-09 09:04:44 +0700

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post users_url, params: { user: { name: 'hung', password: 'secret', password_confirmation: 'secret' } }
+    end
+
+    assert_redirected_to users_url
+  end
+
+  test "should show user" do
+    get user_url(@user)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch user_url(@user), params: { user: { name: @user.name, password: 'secret', password_confirmation: 'secret' } }
+    assert_redirected_to users_url
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+    end
+
+    assert_redirected_to users_url
+  end
+end
